a86c7b981291dd3cc005ac4b6f56af97ae571332
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-14 13:15:51 -0600

    fetch People data

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 import PersonCard from './components/PersonCard'
 
+const peopleURL = "http://localhost:3000/people"
+
 class App extends React.Component {
   state = {
     person: []
+  }
+
+  componentDidMount(){
+    fetch(peopleURL)
+      .then(response => response.json())
+      .then(console.log)
   }
 
   render() {
