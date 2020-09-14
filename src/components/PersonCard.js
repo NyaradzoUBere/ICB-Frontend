b40d3bb1f557576a6e317d20ff6e794e5388de69
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-14 16:27:30 -0600

    add click Function

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import ShowPage from '../pages/Show'
 
 export default function PersonCard(props) {
+    const handleClick = () => {
+        
+       return <ShowPage person = {props.person}/>
+    }
     return(
         <div className = "person-card">
-            <img src= {props.person.image} alt = {props.person.name} className = "person-image"/>
+            <img src= {props.person.image} alt = {props.person.name} className = "person-image" onClick = {handleClick}/>
             <h1>{props.person.name}</h1>
         </div>
     )
