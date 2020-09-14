83c8f75841d64c23efe46f4273dbe1b4e65615b0
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-14 15:01:35 -0600

    provide div for each card

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function PersonCard(props) {
     return(
-        <>
-        <h1>{props.person.name}</h1>
-        <img src= {props.person.image} alt = {props.person.name} className = "person-image"/>
-        </>
+        <div className = "person-card">
+            <img src= {props.person.image} alt = {props.person.name} className = "person-image"/>
+            <h1>{props.person.name}</h1>
+        </div>
     )
 }
