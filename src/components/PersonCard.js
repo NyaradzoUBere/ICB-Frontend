543b0135a47d942beacea45dd5723a28aa9eb055
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-14 14:38:14 -0600

    provide classname for image

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -4,7 +4,7 @@ export default function PersonCard(props) {
     return(
         <>
         <h1>{props.person.name}</h1>
-        <img src= {props.person.image} alt = {props.person.name} />
+        <img src= {props.person.image} alt = {props.person.name} className = "person-image"/>
         </>
     )
 }
