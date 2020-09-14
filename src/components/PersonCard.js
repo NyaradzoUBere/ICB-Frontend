a8f7db8c07f65b1a1ce3ce2755bcc4f0dcdd4bc4
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-14 13:46:13 -0600

    add image to card

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function PersonCard(props) {
     return(
+        <>
         <h1>{props.person.name}</h1>
+        <img src= {props.person.image} alt = {props.person.name} />
+        </>
     )
 }
