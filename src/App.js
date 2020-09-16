0b918eca1f8e2911c3fa58cb80bccc22d996a822
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-15 22:54:57 -0600

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './components/pages/Home'
-import Banner from './components/Banner'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 
 class App extends React.Component {
 
