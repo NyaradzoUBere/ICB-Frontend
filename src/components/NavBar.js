1eb5f0ae6c4e2859e606000ac55024d96f0c52b1
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-16 21:16:25 -0600

    add link to news page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function NavBar() {
     return (
         <div className = "home-nav-bar">
             <ul>
                 <li>Check My Bias</li>
-                <li>In The News</li>
+                <li>
+                    <Link to = {"/news/now"}>In The News</Link>
+                </li>
                 <li>Profile</li>
             </ul>
         </div>
