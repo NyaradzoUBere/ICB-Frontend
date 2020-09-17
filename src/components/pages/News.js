edefe49040d55934125d41e3638a9138af66d5ce
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-16 21:16:06 -0600

    add news page

---
+++ src/components/pages/News.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+
+class News extends React.Component {
+
+    // state = {
+    //     news = []
+    // }
+    render() {
+        return (
+            <div>
+                <h1>News</h1>
+            </div>
+        )
+        
+    }
+}
+
+export default News;
