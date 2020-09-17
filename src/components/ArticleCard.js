8e05c2c7abca56d1de2fa9df42d2076d6f878ddc
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-16 22:26:47 -0600

    rearrange title display

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,9 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function ArticleCard(props) {
+    const article = props.article
     return (
-        <div>
-           <img src = {props.article.urlToImage} />
+        <div className = "article-card">
+           <h3 className = "article-title">{article.title}</h3>
+           <img src = {article.urlToImage} alt = {article.title} className = "article-image"/>
+           <p className = "article-description">{article.description}</p>
         </div>
     )
 }
