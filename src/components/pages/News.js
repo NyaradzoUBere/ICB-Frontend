3ccfd38eee22a2d5d1a97d54ee653e0ccd24a2cd
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-16 21:30:36 -0600

    add function to show each article

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import ArticleCard from '.././ArticleCard'
+
+const newsURL = "http://newsapi.org/v2/everything?q=blm&from=2020-08-17&sortBy=publishedAt&apiKey=540c18062492423c90061fefc752f212"
 
 class News extends React.Component {
 
-    // state = {
-    //     news = []
-    // }
+    state = {
+        news: []
+    }
+
+    componentDidMount() {
+        fetch(newsURL)
+            .then(response => response.json())
+            // .then(response => console.log(response.articles))
+            .then(response => this.setState({news: response.articles}))
+    }
+
+    showArticles = () => {
+        return this.state.news.map(article => {
+            return <ArticleCard article = {article} />
+        })
+    }
     render() {
         return (
-            <div>
-                <h1>News</h1>
-            </div>
+            <>
+                <div className = "news-header">
+                    <h1>News</h1>
+                </div>
+                <div className = "articles">
+                    {this.showArticles()}
+                </div>
+            </>
         )
         
     }
