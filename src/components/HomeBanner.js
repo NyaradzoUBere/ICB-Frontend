514bc48fcb226132255fa18f99a08519f590d467
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-21 13:09:11 -0600

    Change Home title

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Banner() {
     return (
         <div className = "banner">
-            <h1 className= "banner-text">I Still Can't Breathe</h1>
+            <h1 className= "banner-text">I Can't Breathe</h1>
         </div>
     )
 }
