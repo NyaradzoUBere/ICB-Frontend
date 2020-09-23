63b9dfc08ee24de342c8baaf611191534add5ccb
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-23 10:27:05 -0600

    Add question bank

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,254 @@
+var quizQuestions = [
+    {
+        question: "I have pleasant interactions with people of color on a regular basis",
+        answers: [
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I have a friend or close acquaintance who is black",
+        answers: [
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "Over the past few years, the government and news media have shown more respect for black people than they deserve",
+        answers: [
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I would feel nervous if I had to sit alone in a room with a black person and start a conversation",
+        answers: [
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I personally would not want to spend much time with someone who was black",
+        answers: [
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I would not mind if a suitably qualified black person was appointed as my boss",
+        answers: [
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "It should be legal for businesses to refuse to hire black people because of their natural hair looking 'unprofessional'",
+        answers: [
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I wouldn't mind living in a neighborhood where many/all of my neighbors were black",
+        answers: [
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I would experience some anxiety if I were in a room full of black people",
+        answers: [
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Agree"
+            }
+        ]
+    },
+    {
+        question: "I believe names such as 'Jerome' or 'Shanice' are less professional than names such as 'Michael' or 'Emily'",
+        answers: [
+            {
+                type: "Very Bias: Leaning Positive",
+                content: "Strongly Disagree"
+            },
+            {
+                type: "Slightly Bias: Leaning Positive",
+                content: "Slightly Disagree"
+            },
+            {
+                type: "Neutral Bias",
+                content: "Neutral"
+            },
+            {
+                type: "Slightly Bias: Leaning Negative",
+                content: "Slightly Agree"
+            },
+            {
+                type: "Very Bias: Leaning Negative",
+                content: "Strongly Agree"
+            }
+        ]
+    }
+];
+
+export default quizQuestions;
