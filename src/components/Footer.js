631cbfda57ad5a8c164545b317a15b4110918cd5
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-25 12:19:32 -0600

    Provide ICB description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className = "footer">
-            
+            <div className = "footer-description">
+                <p>I Can't Breathe was created in response to the protests happening around the country, sparked by the murder of George Floyd.
+                    The focus is on increasing access to information and taking the first step towards self education and checking one's own bias.
+                </p>
+            </div>
         </div>
     )
 }
