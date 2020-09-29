63d2857b9ee81b5783325678afb43e5b8facf512
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-29 17:07:15 -0600

    remove responsiveNavBar function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function NavBar() {
-    const responsiveNavBar = () => {
-        let x = document.getElementById("my-nav-bar");
-        if (x.className === "nav-bar") {
-          x.className += "responsive";
-        } else {
-          x.className = "nav-bar";
-        }
-      }
+    // const responsiveNavBar = () => {
+    //     let x = document.getElementById("my-nav-bar");
+    //     if (x.className === "nav-bar") {
+    //       x.className += "responsive";
+    //     } else {
+    //       x.className = "nav-bar";
+    //     }
+    //   }
 
     return (
         <div className = "nav-bar" id="my-nav-bar">
@@ -23,11 +23,11 @@ export default function NavBar() {
                 <li>
                     <Link to ="/news/now">In The News</Link>
                 </li>
-                <li>
+                {/* <li>
                     <a href="javascript:void(0);" class="icon" onClick={responsiveNavBar}>
                         <i class="fa fa-bars"></i>
                     </a>
-                </li>
+                </li> */}
             </ul>
         </div>
     )
