d03d3a25f97937ca17512d89951d9e201383e91a
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-28 20:05:49 -0600

    change nav bar alignment based on screen size

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,31 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function NavBar() {
+    const responsiveNavBar = () => {
+        let x = document.getElementById("my-nav-bar");
+        if (x.className === "nav-bar") {
+          x.className += "responsive";
+        } else {
+          x.className = "nav-bar";
+        }
+      }
+
     return (
-        <div className = "nav-bar">
+        <div className = "nav-bar" id="my-nav-bar">
             <ul>
                 <li>
-                    <Link to ="/">Home</Link>
+                    <Link to ="/" className = "active">Home</Link>
                 </li>
                 <li>
                     <Link to ="/quiz/self">Check My Bias</Link>
                 </li>
                 <li>
                     <Link to ="/news/now">In The News</Link>
+                </li>
+                <li>
+                    <a href="javascript:void(0);" class="icon" onClick={responsiveNavBar}>
+                        <i class="fa fa-bars"></i>
+                    </a>
                 </li>
             </ul>
         </div>
