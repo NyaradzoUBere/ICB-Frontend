99d44b0a55c33f734758c1c1a0fe38783f62603b
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-29 16:57:35 -0600

    style footer with min width 501

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import logo from './icblogo copy.png'
 export default function Footer() {
     return (
         <div className = "footer">
-            <div className = "footer-image">
-                <img src = {logo} alt = "I Can't Breathe logo" />
+            <div className = "footer-image-div">
+                <img src = {logo} alt = "I Can't Breathe logo" className = "footer-image"/>
             </div>
             <div className = "footer-description">
                 <p>I Can't Breathe was created in response to the protests happening around the country, sparked by the murder of George Floyd.
