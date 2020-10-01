fa73cca654fadc91ffb6374c9de11d8cd11904ea
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-30 21:46:44 -0600

    Render ICB logo when article doesn't have image

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from './darkicblogo.png'
 
 export default function ArticleCard(props) {
     const article = props.article
@@ -6,7 +7,11 @@ export default function ArticleCard(props) {
         <div className = "article-card">
            <h3 className = "article-title">{article.title}</h3>
            <a href={article.url} target="_blank" rel="noopener noreferrer">
-               <img src = {article.urlToImage} alt = {article.title} className = "article-image"/>
+               {article.urlToImage ? (
+                   <img src = {article.urlToImage} alt = {article.title} className = "article-image"/>
+                   ) : (
+                    <img src = {logo} alt = {article.title} className = "article-image"/>
+                    )}
            </a>
            <p className = "article-description">{article.description}</p>
         </div>
