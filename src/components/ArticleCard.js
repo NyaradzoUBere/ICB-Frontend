b8fd694b0ee2d0f1e0328a239799fcdc69594ccc
NyaradzoUBere <nybe5598@colorado.edu>
2020-09-30 21:50:59 -0600

    wrap entire card in anchor

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -5,15 +5,15 @@ export default function ArticleCard(props) {
     const article = props.article
     return (
         <div className = "article-card">
-           <h3 className = "article-title">{article.title}</h3>
-           <a href={article.url} target="_blank" rel="noopener noreferrer">
-               {article.urlToImage ? (
-                   <img src = {article.urlToImage} alt = {article.title} className = "article-image"/>
-                   ) : (
-                    <img src = {logo} alt = {article.title} className = "article-image"/>
-                    )}
-           </a>
-           <p className = "article-description">{article.description}</p>
+            <a href={article.url} target="_blank" rel="noopener noreferrer">
+                <h3 className = "article-title">{article.title}</h3>
+                    {article.urlToImage ? (
+                        <img src = {article.urlToImage} alt = {article.title} className = "article-image"/>
+                        ) : (
+                            <img src = {logo} alt = {article.title} className = "article-image"/>
+                            )}
+                <p className = "article-description">{article.description}</p>
+            </a>
         </div>
     )
 }
