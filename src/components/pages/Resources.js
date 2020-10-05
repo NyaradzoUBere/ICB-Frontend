df7b373939670bcb0f2494b28d52226c2aa1c201
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 15:26:24 -0600

    add scroll to top function

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -2,22 +2,25 @@ import React from 'react'
 
 export default function Resources() {
     return (
-        <div className = "resources-page">
-            <div className = "resources-title">
-                <h1>Resources</h1>
-                <div className = "movies-resources">
-                    <h2>Movies</h2>
-                </div>
-                <div className = "shows-resources">
-                    <h2>TV Shows</h2>
-                </div>
-                <div className = "books-resources">
-                    <h2>Books</h2>
-                </div>
-                <div className = "podcasts-resources">
-                    <h2>Podcasts</h2>
+        <>
+        {window.scrollTo(0, 0)}
+            <div className = "resources-page">
+                <div className = "resources-title">
+                    <h1>Resources</h1>
+                    <div className = "movies-resources">
+                        <h2>Movies</h2>
+                    </div>
+                    <div className = "shows-resources">
+                        <h2>TV Shows</h2>
+                    </div>
+                    <div className = "books-resources">
+                        <h2>Books</h2>
+                    </div>
+                    <div className = "podcasts-resources">
+                        <h2>Podcasts</h2>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
