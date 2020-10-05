120e011e6cd3857d0f1abb9585441cef63d7778a
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 15:39:30 -0600

    add inital books list

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -30,6 +30,8 @@ export default function Resources() {
                     <div className = "shows-resources">
                         <h2 className = "shows-title">TV Shows</h2>
                         <ul className = "shows-list">
+                            <li className = "show">Dear White People</li>
+                            <li className = "show">When They See Us</li>
                             <li className = "show"></li>
                             <li className = "show"></li>
                             <li className = "show"></li>
@@ -38,6 +40,26 @@ export default function Resources() {
                     <div className = "books-resources">
                         <h2 className = "books-title">Books</h2>
                         <ul className = "books-list">
+                            <li className = "book">Black Feminist Thought</li>
+                            <li className = "book">Eloquent Rage: A Black Feminist Discovers Her Superpower</li>
+                            <li className = "book">Heavy: An American Memoir</li>
+                            <li className = "book">How To Be An Antiracist</li>
+                            <li className = "book">I Know Why the Caged Bird Sings</li>
+                            <li className = "book">Invisible No More: Police Violence Against Black Women and Women of Color</li>
+                            <li className = "book">Just Mercy</li>
+                            <li className = "book">Me and White Supremacy</li>
+                            <li className = "book">Raising Our Hands</li>
+                            <li className = "book">Redefining Realness</li>
+                            <li className = "book">Sister Outsider</li>
+                            <li className = "book">So You Want to Talk About Race</li>
+                            <li className = "book">The Bluest Eye</li>
+                            <li className = "book">The Fire Next Time</li>
+                            <li className = "book">The New Jim Crow: Mass Incarceration in the Age of Colorblindness</li>
+                            <li className = "book">The Next American Revolution: Sustainable Activism for the Twenty-First Century</li>
+                            <li className = "book">When Affirmative Action Was White: An Untold History of Racial Inequality in Twentieth-Century America</li>
+                            <li className = "book">White Fragility: Why It's So Hard for White People to Talk About Racism</li>
+                            <li className = "book"></li>
+                            <li className = "book"></li>
                             <li className = "book"></li>
                         </ul>
                     </div>
