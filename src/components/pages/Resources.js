548e33488fa9d5af1b59c561dac54aa02afcf132
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 15:35:39 -0600

    add initial movies list

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -10,12 +10,28 @@ export default function Resources() {
                     <div className = "movies-resources">
                         <h2 className = "movies-title">Movies</h2>
                         <ul className = "movies-list">
+                            <li className = "movie">13th</li>
+                            <li className = "movie">American Son</li>
+                            <li className = "movie">Blindspotting</li>
+                            <li className = "movie">Clemency</li>
+                            <li className = "movie">Fruitvale Station</li>
+                            <li className = "movie">I Am Not Your Negro</li>
+                            <li className = "movie">If Beale Street Could Talk</li>
+                            <li className = "movie">Just Mercy</li>
+                            <li className = "movie">King In The Wilderness</li>
+                            <li className = "movie">See You Yesterday</li>
+                            <li className = "movie">Selma</li>
+                            <li className = "movie">The Black Panthers: Vanguard of the Revolution</li>
+                            <li className = "movie">The Hate U Give</li>
+                            <li className = "movie"></li>
                             <li className = "movie"></li>
                         </ul>
                     </div>
                     <div className = "shows-resources">
                         <h2 className = "shows-title">TV Shows</h2>
                         <ul className = "shows-list">
+                            <li className = "show"></li>
+                            <li className = "show"></li>
                             <li className = "show"></li>
                         </ul>
                     </div>
