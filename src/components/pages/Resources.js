b1853df68cfafbb11c0ea4225bf31767c5770dca
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 15:30:47 -0600

    add list framework

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -8,16 +8,28 @@ export default function Resources() {
                 <div className = "resources-title">
                     <h1>Resources</h1>
                     <div className = "movies-resources">
-                        <h2>Movies</h2>
+                        <h2 className = "movies-title">Movies</h2>
+                        <ul className = "movies-list">
+                            <li className = "movie"></li>
+                        </ul>
                     </div>
                     <div className = "shows-resources">
-                        <h2>TV Shows</h2>
+                        <h2 className = "shows-title">TV Shows</h2>
+                        <ul className = "shows-list">
+                            <li className = "show"></li>
+                        </ul>
                     </div>
                     <div className = "books-resources">
-                        <h2>Books</h2>
+                        <h2 className = "books-title">Books</h2>
+                        <ul className = "books-list">
+                            <li className = "book"></li>
+                        </ul>
                     </div>
                     <div className = "podcasts-resources">
-                        <h2>Podcasts</h2>
+                        <h2 className = "podcasts-title">Podcasts</h2>
+                        <ul className = "podcast-list">
+                            <li className = "podcast"></li>
+                        </ul>
                     </div>
                 </div>
             </div>
