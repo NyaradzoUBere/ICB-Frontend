f685daf0d0e18440c1b53bea9953d1a66982e83b
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 16:49:15 -0600

    add and style resources cards

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -9,25 +9,27 @@ export default function Resources() {
                 <div className = "resources-header">
                     <h1>Resources</h1>
                 </div>
-                <div className = "movie-card">
-                    <li>
-                        <Link to ="/resources/movies">Movies</Link>
-                    </li>
-                </div>
-                <div className = "shows-card">
-                    <li>
-                        <Link to ="/resources/tvshows">TV Shows</Link>
-                    </li>
-                </div>
-                <div className = "books-card">
-                    <li>
-                        <Link to ="/resources/books">Books</Link>
-                    </li>
-                </div>
-                <div className = "podcasts-card">
-                    <li>
-                        <Link to ="/resources/podcasts">Podcasts</Link>
-                    </li>
+                <div className = "resources-cards">
+                    <Link to ="/resources/movies" className = "title">
+                    <div className = "movie-card">
+                        <h3 className = "title">Movies</h3>
+                    </div>
+                    </Link>
+                    <Link to ="/resources/tvshows" className = "title">
+                    <div className = "shows-card">
+                    <h3 className = "title">TV Shows</h3>
+                    </div>
+                    </Link>
+                    <Link to ="/resources/books" className = "title">
+                    <div className = "books-card">
+                        <h3 className = "title">Books</h3>
+                    </div>
+                    </Link>
+                    <Link to ="/resources/podcasts" className = "title">
+                        <div className = "podcasts-card">
+                            <h3 className = "title">Podcasts</h3>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </>
