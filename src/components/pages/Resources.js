f6f182504bee7e243ee0c7eb12b74ebdab717071
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 15:58:57 -0600

    initial resources page styling and add bank movies only page

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -5,68 +5,68 @@ export default function Resources() {
         <>
         {window.scrollTo(0, 0)}
             <div className = "resources-page">
-                <div className = "resources-title">
+                <div className = "resources-header">
                     <h1>Resources</h1>
-                    <div className = "movies-resources">
-                        <h2 className = "movies-title">Movies</h2>
-                        <ul className = "movies-list">
-                            <li className = "movie">13th</li>
-                            <li className = "movie">American Son</li>
-                            <li className = "movie">Blindspotting</li>
-                            <li className = "movie">Clemency</li>
-                            <li className = "movie">Fruitvale Station</li>
-                            <li className = "movie">I Am Not Your Negro</li>
-                            <li className = "movie">If Beale Street Could Talk</li>
-                            <li className = "movie">Just Mercy</li>
-                            <li className = "movie">King In The Wilderness</li>
-                            <li className = "movie">See You Yesterday</li>
-                            <li className = "movie">Selma</li>
-                            <li className = "movie">The Black Panthers: Vanguard of the Revolution</li>
-                            <li className = "movie">The Hate U Give</li>
-                        </ul>
-                    </div>
-                    <div className = "shows-resources">
-                        <h2 className = "shows-title">TV Shows</h2>
-                        <ul className = "shows-list">
-                            <li className = "show">Dear White People</li>
-                            <li className = "show">When They See Us</li>
-                        </ul>
-                    </div>
-                    <div className = "books-resources">
-                        <h2 className = "books-title">Books</h2>
-                        <ul className = "books-list">
-                            <li className = "book">Black Feminist Thought</li>
-                            <li className = "book">Eloquent Rage: A Black Feminist Discovers Her Superpower</li>
-                            <li className = "book">Heavy: An American Memoir</li>
-                            <li className = "book">How To Be An Antiracist</li>
-                            <li className = "book">I Know Why the Caged Bird Sings</li>
-                            <li className = "book">Invisible No More: Police Violence Against Black Women and Women of Color</li>
-                            <li className = "book">Just Mercy</li>
-                            <li className = "book">Me and White Supremacy</li>
-                            <li className = "book">Raising Our Hands</li>
-                            <li className = "book">Redefining Realness</li>
-                            <li className = "book">Sister Outsider</li>
-                            <li className = "book">So You Want to Talk About Race</li>
-                            <li className = "book">The Bluest Eye</li>
-                            <li className = "book">The Fire Next Time</li>
-                            <li className = "book">The New Jim Crow: Mass Incarceration in the Age of Colorblindness</li>
-                            <li className = "book">The Next American Revolution: Sustainable Activism for the Twenty-First Century</li>
-                            <li className = "book">When Affirmative Action Was White: An Untold History of Racial Inequality in Twentieth-Century America</li>
-                            <li className = "book">White Fragility: Why It's So Hard for White People to Talk About Racism</li>
-                        </ul>
-                    </div>
-                    <div className = "podcasts-resources">
-                        <h2 className = "podcasts-title">Podcasts</h2>
-                        <ul className = "podcast-list">
-                            <li className = "podcast">Code Switch (NPR)</li>
-                            <li className = "podcast">1619 (New York Times)</li>
-                            <li className = "podcast">About Race</li>
-                            <li className = "podcast">Intersectionality Matters! hosted by Kimberlé Crenshaw</li>
-                            <li className = "podcast">Momentum: A Race Forward Podcast</li>
-                            <li className = "podcast">Nice White Parents (Serial and The New York Times)</li>
-                            <li className = "podcast">Seeing White</li>
-                        </ul>
-                    </div>
+                </div>
+                <div className = "movies-resources">
+                    <h2 className = "resources-title">Movies</h2>
+                    <ul className = "resources-list">
+                        <li className = "resource">13th</li>
+                        <li className = "resource">American Son</li>
+                        <li className = "resource">Blindspotting</li>
+                        <li className = "resource">Clemency</li>
+                        <li className = "resource">Fruitvale Station</li>
+                        <li className = "resource">I Am Not Your Negro</li>
+                        <li className = "resource">If Beale Street Could Talk</li>
+                        <li className = "resource">Just Mercy</li>
+                        <li className = "resource">King In The Wilderness</li>
+                        <li className = "resource">See You Yesterday</li>
+                        <li className = "resource">Selma</li>
+                        <li className = "resource">The Black Panthers: Vanguard of the Revolution</li>
+                        <li className = "resource">The Hate U Give</li>
+                    </ul>
+                </div>
+                <div className = "shows-resources">
+                    <h2 className = "resources-title">TV Shows</h2>
+                    <ul className = "resources-list">
+                        <li className = "resource">Dear White People</li>
+                        <li className = "resource">When They See Us</li>
+                    </ul>
+                </div>
+                <div className = "books-resources">
+                    <h2 className = "resources-title">Books</h2>
+                    <ul className = "resources-list">
+                        <li className = "resource">Black Feminist Thought</li>
+                        <li className = "resource">Eloquent Rage: A Black Feminist Discovers Her Superpower</li>
+                        <li className = "resource">Heavy: An American Memoir</li>
+                        <li className = "resource">How To Be An Antiracist</li>
+                        <li className = "resource">I Know Why the Caged Bird Sings</li>
+                        <li className = "resource">Invisible No More: Police Violence Against Black Women and Women of Color</li>
+                        <li className = "resource">Just Mercy</li>
+                        <li className = "resource">Me and White Supremacy</li>
+                        <li className = "resource">Raising Our Hands</li>
+                        <li className = "resource">Redefining Realness</li>
+                        <li className = "resource">Sister Outsider</li>
+                        <li className = "resource">So You Want to Talk About Race</li>
+                        <li className = "resource">The Bluest Eye</li>
+                        <li className = "resource">The Fire Next Time</li>
+                        <li className = "resource">The New Jim Crow: Mass Incarceration in the Age of Colorblindness</li>
+                        <li className = "resource">The Next American Revolution: Sustainable Activism for the Twenty-First Century</li>
+                        <li className = "resource">When Affirmative Action Was White: An Untold History of Racial Inequality in Twentieth-Century America</li>
+                        <li className = "resource">White Fragility: Why It's So Hard for White People to Talk About Racism</li>
+                    </ul>
+                </div>
+                <div className = "podcasts-resources">
+                    <h2 className = "resources-title">Podcasts</h2>
+                    <ul className = "resources-list">
+                        <li className = "resource">Code Switch (NPR)</li>
+                        <li className = "resource">1619 (New York Times)</li>
+                        <li className = "resource">About Race</li>
+                        <li className = "resource">Intersectionality Matters! hosted by Kimberlé Crenshaw</li>
+                        <li className = "resource">Momentum: A Race Forward Podcast</li>
+                        <li className = "resource">Nice White Parents (Serial and The New York Times)</li>
+                        <li className = "resource">Seeing White</li>
+                    </ul>
                 </div>
             </div>
         </>
