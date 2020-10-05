16834e1d5a4301b4eb0c9e5c25294408bfda418e
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 15:25:35 -0600

    add resources framework

--- src/components/pages/Resources.js
+++ src/components/pages/Resources.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Resources() {
     return (
-        <div>
-            
+        <div className = "resources-page">
+            <div className = "resources-title">
+                <h1>Resources</h1>
+                <div className = "movies-resources">
+                    <h2>Movies</h2>
+                </div>
+                <div className = "shows-resources">
+                    <h2>TV Shows</h2>
+                </div>
+                <div className = "books-resources">
+                    <h2>Books</h2>
+                </div>
+                <div className = "podcasts-resources">
+                    <h2>Podcasts</h2>
+                </div>
+            </div>
         </div>
     )
 }
