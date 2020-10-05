3939f9992f7d05529a8a13ec16d8cf3ade8d86d6
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-05 17:25:02 -0600

    add links to each resource

--- src/components/pages/Movies.js
+++ src/components/pages/Movies.js
@@ -5,19 +5,45 @@ export default function Movies() {
     <div className = "resources">
         <h2 className = "resources-title">Movies</h2>
         <ul className = "resources-list">
-            <li className = "resource">13th</li>
-            <li className = "resource">American Son</li>
-            <li className = "resource">Blindspotting</li>
-            <li className = "resource">Clemency</li>
-            <li className = "resource">Fruitvale Station</li>
-            <li className = "resource">I Am Not Your Negro</li>
-            <li className = "resource">If Beale Street Could Talk</li>
-            <li className = "resource">Just Mercy</li>
-            <li className = "resource">King In The Wilderness</li>
-            <li className = "resource">See You Yesterday</li>
-            <li className = "resource">Selma</li>
-            <li className = "resource">The Black Panthers: Vanguard of the Revolution</li>
-            <li className = "resource">The Hate U Give</li>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?hl=en&sxsrf=ALeKk03wnLXY_-yvT3yR6DgQSp2WkIYfPg%3A1601939375969&source=hp&ei=r6d7X-P6N4--tAbW1ZDYCw&q=13th&oq=13th&gs_lcp=CgZwc3ktYWIQAzIICAAQsQMQgwEyDgguELEDEIMBEMcBEK8BMgUIABCxAzIFCC4QsQMyCAgAELEDEIMBMgUILhCxAzIOCC4QsQMQgwEQxwEQrwEyBQgAELEDMggIABCxAxCDATIICAAQsQMQgwE6CwguEMcBEKMCEJMCOgIIADoICC4QxwEQrwE6CwguELEDEMcBEKMCOggILhCxAxCDAToICC4QsQMQkwI6AgguUKYGWP8UYIkXaANwAHgAgAFwiAHsBJIBAzYuMZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwijjtCbyZ7sAhUPH80KHdYqBLsQ4dUDCAk&uact=5">
+                <li className = "resource">13th</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=american+son&rlz=1C5CHFA_enUS721US727&oq=american+son&aqs=chrome.0.0j46j0l5j46.1844j0j4&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">American Son</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=Blindspotting&rlz=1C5CHFA_enUS721US727&oq=blin&aqs=chrome.0.69i59j69i57j0j46j0j46l2j0.1963j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">Blindspotting</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?rlz=1C5CHFA_enUS721US727&sxsrf=ALeKk03E1pbtfZzg114sKch5JeNYtKHjbQ%3A1601939794490&ei=Uql7X9ioHdX5tAaP5q3oCg&q=clemency+chinonye+chukwu&oq=clemency+chinonye+chukwu&gs_lcp=CgZwc3ktYWIQAzIFCC4QkwIyBggAEBYQHjIGCAAQFhAeOgQIABBHOgwIABCxAxBDEEYQ-QE6BQguELEDOgUIABCxAzoECAAQQzoCCAA6CAguELEDEJMCOgIILjoECAAQCjoICAAQFhAKEB5QlSVYzoUBYLqIAWgFcAJ4AIAB6AGIAcUPkgEGMTUuNS4xmAEAoAEBqgEHZ3dzLXdpesgBCMABAQ&sclient=psy-ab&ved=0ahUKEwjYmZvjyp7sAhXVPM0KHQ9zC60Q4dUDCA0&uact=5">
+                <li className = "resource">Clemency</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=fruitvale+station&rlz=1C5CHFA_enUS721US727&oq=Fruitvale+Station&aqs=chrome.0.0j46j0l6.741j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">Fruitvale Station</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href ="https://www.google.com/search?gs_ssp=eJzj4tVP1zc0TDYsKqtIiTc1YPSSyFRIzFXIyy9RqMwvLVLIS00vyldIys_PBgAJIA17&q=i+am+not+your+negro+book&rlz=1C5CHFA_enUS721US727&oq=i+am+not+your+negro&aqs=chrome.0.46j69i57j0l5.3408j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">I Am Not Your Negro</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=if+beale+street+could+talk&rlz=1C5CHFA_enUS721US727&oq=if+&aqs=chrome.0.69i59j69i57j69i59j46j0j69i60l3.1174j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">If Beale Street Could Talk</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href ="https://www.google.com/search?q=just+mercy&rlz=1C5CHFA_enUS721US727&oq=just+mercy&aqs=chrome..69i57j46l2j0l5.1694j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">Just Mercy</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=King+In+The+Wilderness&rlz=1C5CHFA_enUS721US727&oq=King+In+The+Wilderness&aqs=chrome..69i57j69i59j69i64.311j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">King In The Wilderness</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=See+You+Yesterday&rlz=1C5CHFA_enUS721US727&oq=see+y&aqs=chrome.0.69i59j46j69i57j0l2j69i60l3.844j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">See You Yesterday</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=selma&rlz=1C5CHFA_enUS721US727&oq=selma&aqs=chrome..69i57j46l2j0j46l2j0l2.1402j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">Selma</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href = "https://www.google.com/search?q=The+Black+Panthers%3A+Vanguard+of+the+Revolution&rlz=1C5CHFA_enUS721US727&oq=the+bl&aqs=chrome.0.69i59j69i57j0j46l2j69i60l3.1084j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">The Black Panthers: Vanguard of the Revolution</li>
+            </a>
+            <a target = "_blank" rel = "noopener noreferrer" href ="https://www.google.com/search?q=the+hate+u+give&rlz=1C5CHFA_enUS721US727&oq=the+hate+u+give&aqs=chrome.0.0j46l2j0l5.4385j0j9&sourceid=chrome&ie=UTF-8">
+                <li className = "resource">The Hate U Give</li>
+            </a>
         </ul>
     </div>
     )
